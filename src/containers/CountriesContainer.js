825afbae0a32f60870202ace6023336316e7eaa1
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-03-25 14:09:49 +0000

    Create container file

---
+++ src/containers/CountriesContainer.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const CountriesContainer = () => {
+
+    const [countries, setCountries] = useState(null);
+    
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default CountriesContainer;
