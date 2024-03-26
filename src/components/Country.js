7b9a4df535c83debda5d1f5f1465fb1259ed28ac
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-03-26 08:48:36 +0000

    Add extra information to each country

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,8 +3,11 @@ const Country = ({country, addVisitedCountries}) => {
     return ( 
         <>
             <li>
-                {country.name.common} 
-                <p>{country.capital}</p>
+                {country.name.common} {country.flag}
+                <p>Capital: {country.capital}</p>
+                <p>
+                    <a href={country.maps.googleMaps}>Google Maps</a>
+                </p>
             </li>
       
       <button onClick={() => addVisitedCountries(country)}> Visited </button>
